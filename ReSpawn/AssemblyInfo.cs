4f8d4b27a93b0b55b490786c668c43ea8afaad9d
Harshit Singhal <harshitsinghal.dev@outlook.com>
2026-06-14 00:52:15 +0530

    released-v1.2.0

--- ReSpawn/AssemblyInfo.cs
+++ ReSpawn/AssemblyInfo.cs
@@ -1,12 +1,12 @@
 using System.Reflection;
 using System.Windows;
 
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
 [assembly: AssemblyProduct("ReSpawn")]
 [assembly: AssemblyCompany("Harshit Singhal")]
-[assembly: AssemblyCopyright("© 2026 Harshit Singhal")]
+[assembly: AssemblyCopyright("ï¿½ 2026 Harshit Singhal")]
 [assembly: AssemblyDescription("Unified game library and playtime tracker")]
 
 [assembly: ThemeInfo(
